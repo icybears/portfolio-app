820830b52626afe260270377e437cb26b789d137
I. Saber <isaber23@outlook.com>
2018-09-06 17:50:53 +0100

    Auth + Mail integration

--- database/migrations/2018_09_06_141653_create_panels_table.php
+++ database/migrations/2018_09_06_141653_create_panels_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class CreatePanelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('panels', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->text('title');
+            $table->text('content');
         });
     }
 
@@ -26,6 +28,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('panels');
     }
 }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Sabercrafts</title>
 </head>
 
@@ -147,7 +147,7 @@
         Footer stuff
     </footer>
 </body>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/app.js"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 
 </html>
